6ce9aff7ebd2b83b510b9eaf06fc730ff4d0e910
saqlainAbbas06 <saqlainabbass862@gmail.com>
2026-09-03 02:34:48 +0500

    implement merge sorted algorithm

---
+++ DSA/merge_sorted_alg.js
@@ -0,0 +1,47 @@
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return;
+  }
+  
+  const middlePoint = Math.floor(array.length / 2);
+  const leftPart = array.slice(0, middlePoint);
+  const rightPart = array.slice(middlePoint);
+  
+  mergeSort(leftPart);
+  mergeSort(rightPart);
+  
+  let leftArrayIndex = 0;
+  let rightArrayIndex = 0;
+  let sortedIndex = 0;
+  
+  while (leftArrayIndex < leftPart.length && rightArrayIndex < rightPart.length) {
+    if (leftPart[leftArrayIndex] < rightPart[rightArrayIndex]) {
+      array[sortedIndex] = leftPart[leftArrayIndex];
+      leftArrayIndex += 1;
+    } else {
+      array[sortedIndex] = rightPart[rightArrayIndex];
+      rightArrayIndex += 1;
+    }
+    sortedIndex += 1;
+  }
+  
+  while (leftArrayIndex < leftPart.length) {
+    array[sortedIndex] = leftPart[leftArrayIndex];
+  
+    leftArrayIndex += 1;
+    sortedIndex += 1;
+  }
+  
+  while (rightArrayIndex < rightPart.length) {
+    array[sortedIndex] = rightPart[rightArrayIndex];
+    rightArrayIndex += 1;
+    sortedIndex += 1;
+  }
+}
+
+const numbers = [4, 10, 6, 14, 2, 1, 8, 5];
+console.log('Unsorted array: ');
+console.log(numbers);
+mergeSort(numbers);
+console.log('Sorted array: ');
+console.log(numbers)
