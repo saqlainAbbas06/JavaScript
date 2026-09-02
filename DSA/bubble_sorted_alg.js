49917698e327fee319c2dec11709aee7fa3f1a65
saqlainAbbas06 <saqlainabbass862@gmail.com>
2026-09-03 03:07:41 +0500

    implement bubble sorted algorithm

---
+++ DSA/bubble_sorted_alg.js
@@ -0,0 +1,21 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let swapped = false;
+
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {      
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;             
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return array;
+}
+
+const numbers = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+console.log(bubbleSort(numbers));
